78f799f3577fa69f4ac7cdf191273bfb9d658bd0
Fernando <fernando.alvarez@islas.org.mx>
2021-12-09 12:23:51 -0800

    🎨 Aplica black a scripts

--- cat_recognition/resize_images.py
+++ cat_recognition/resize_images.py
@@ -1,8 +1,13 @@
 from PIL import Image
 
 
-def resize_image(original_path: str, destionation_path: str, width: int = 256,
-                 height: int = 256, scale_filter=Image.ANTIALIAS):
+def resize_image(
+    original_path: str,
+    destionation_path: str,
+    width: int = 256,
+    height: int = 256,
+    scale_filter=Image.ANTIALIAS,
+):
     original_image = Image.open(original_path)
     resized_image = original_image.resize((width, height), scale_filter)
     resized_image.save(destionation_path)
@@ -15,12 +20,12 @@ if __name__ == "__main__":
 
     original_images_path = "./data/originals/"
     resized_image_path = "./data/resized/"
-    images_list = [original_images_path +
-                   file_name for file_name in os.listdir(original_images_path)]
+    images_list = [
+        original_images_path + file_name for file_name in os.listdir(original_images_path)
+    ]
 
     if not os.path.exists(resized_image_path):
         os.mkdir(resized_image_path)
 
     for image in images_list:
-        resize_image(image, os.path.join(
-            resized_image_path, os.path.split(image)[1]))
+        resize_image(image, os.path.join(resized_image_path, os.path.split(image)[1]))
