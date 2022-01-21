ed1741e56518f420ed7a5aa6e285791d472b5179
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-01-21 13:52:24 -0800

    🚧Improve variable name

--- src/first_recognition.py
+++ src/first_recognition.py
@@ -25,7 +25,7 @@ detector.loadModel()
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 image_paths = [f"{files[:-4]}.jpg" for files in onlyfiles]
 processed_image_paths = [f"{files[:-4]}_predicted.jpg" for files in onlyfiles]
-output_image_paths = [
+paths_of_processed_image_as_negative = [
     os.path.join(negative_detections_path, processed_image_path)
     for processed_image_path in processed_image_paths
 ]
@@ -35,14 +35,14 @@ paths_of_processed_image_as_positive = [
     for processed_image_path in processed_image_paths
 ]
 custom_objects = detector.CustomObjects(cat=True)
-inputs_and_paths = zip(input_images, output_image_paths, paths_of_processed_image_as_positive)
-for input_image, output_image_path, processed_as_positive in inputs_and_paths:
+inputs_and_paths = zip(input_images, paths_of_processed_image_as_negative, paths_of_processed_image_as_positive)
+for input_image, processed_as_positive, processed_as_positive in inputs_and_paths:
     detections = detector.detectCustomObjectsFromImage(
         minimum_percentage_probability=1,
         custom_objects=custom_objects,
         input_image=input_image,
-        output_image_path=output_image_path,
+        output_image_path=processed_as_positive,
     )
     name_objects = [individual_detection["name"] for individual_detection in detections]
     if "cat" in name_objects:
-        os.replace(output_image_path, processed_as_positive)
+        os.replace(processed_as_positive, processed_as_positive)
