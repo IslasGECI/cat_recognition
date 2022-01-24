3648103bf17513cd465ab19006f1769c12784e58
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-01-24 14:21:57 -0800

    🚨 Follow flake8

--- tests/test_first_reconigtion.py
+++ tests/test_first_reconigtion.py
@@ -4,10 +4,10 @@ import cat_recognition as cr
 
 class Test_Cat_Detector:
     def test_init(self):
-        cat_detector = cr.Cat_Detector()
+        cr.Cat_Detector()
 
 
 class Test_Paths_Management:
     def test_init(self):
         workdir = os.getcwd()
-        cat_detector = cr.Paths_Management(workdir)
+        cr.Paths_Management(workdir)
