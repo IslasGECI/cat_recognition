47cfdc53158ff7bcdb931803ec8041da84ba6836
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-01-24 14:10:03 -0800

    ✅ 🏹👾 Test method init
    Class Paths_Management

--- tests/test_first_reconigtion.py
+++ tests/test_first_reconigtion.py
@@ -1,3 +1,4 @@
+import os
 import cat_recognition as cr
 
 
@@ -10,3 +11,9 @@ def test_return_one():
 class Test_Cat_Detector:
     def test_init(self):
         cat_detector = cr.Cat_Detector()
+
+
+class Test_Paths_Management:
+    def test_init(self):
+        workdir = os.getcwd()
+        cat_detector = cr.Paths_Management(workdir)
