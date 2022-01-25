ea1038cf372af196759f1ed34076073dd8004c9a
Xavier Paredes <jxavierpp1@gmail.com>
2022-01-25 18:03:55 +0000

    ✅ Add passing test get_input_output_paths

--- tests/test_first_reconigtion.py
+++ tests/test_first_reconigtion.py
@@ -1,5 +1,6 @@
 import os
 import cat_recognition as cr
+from cat_recognition.first_recognition import Paths_Management
 
 
 class Test_Cat_Detector:
@@ -11,3 +12,10 @@ class Test_Paths_Management:
     def test_init(self):
         workdir = os.getcwd()
         cr.Paths_Management(workdir)
+
+    def test_get_input_output_paths(self):
+        execution_path = os.getcwd()
+        paths_management = Paths_Management(execution_path)
+        paths_management.setup_directory_processed()
+        inputs_and_paths = paths_management.get_input_output_paths()
+        assert inputs_and_paths.__class__ == type(zip())
