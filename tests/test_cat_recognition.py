b12c9f49156b94d73c2b66e6ad62ebbb2c90ac02
Mario Villasante <mario.villasante@islas.org.mx>
2022-10-14 14:36:13 -0700

    🏹👾 Catch mutant

--- tests/test_cat_recognition.py
+++ tests/test_cat_recognition.py
@@ -1,5 +1,7 @@
-import cat_recognition as cr
+import hashlib
 import os
+
+import cat_recognition as cr
 
 
 def test_resize_image():
@@ -7,3 +9,8 @@ def test_resize_image():
     resized_image_path = "tests/data/image3_resized.jpg"
     cr.resize_image(original_image_path, resized_image_path)
     assert os.path.isfile(resized_image_path)
+
+    image = open(resized_image_path, "rb").read()
+    obtained_hash = hashlib.md5(image).hexdigest()
+    expected_hash = "12f1ac191833ce9a8ad3e9482545176d"
+    assert obtained_hash == expected_hash
