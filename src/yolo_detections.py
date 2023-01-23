6b467ba51078dc0a528e504863b2dc1e35da05e1
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-01-23 11:41:24 -0800

    🚧 Add first version of `yolo_detections`

---
+++ src/yolo_detections.py
@@ -0,0 +1,50 @@
+import cv2 as cv
+import numpy as np
+from wandb import Classes
+
+# load yolo
+net = cv.dnn.readNet("yolov3.weights", "cfg/yolov3.cfg")
+clasees = []
+with open("data/coco.names", 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+# print(classes)
+layer_name = net.getLayerNames()
+output_layer = [layer_name[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Load Image
+img = cv.imread("../data/raw/IMG_0270.jpg")
+img = cv.resize(img, None, fx=0.4, fy=0.4)
+height, width, channel = img.shape
+
+# Detect Objects
+blob = cv.dnn.blobFromImage(
+    img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layer)
+
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.01:
+            # Object detection
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+            # cv.circle(img, (center_x, center_y), 10, (0, 255, 0), 2 )
+            # Reactangle Cordinate
+            x = int(center_x - w/2)
+            y = int(center_y - h/2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+
+for class_id in class_ids:
+    print(classes[class_id])
