6e8c979e8d5b14fa44733182368ad6a7a64e4be7
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-20 12:21:12 -0800

    🎨 Format with Styler

--- cat_recognition/yolo_detections.py
+++ cat_recognition/yolo_detections.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 
+
 def classify_from_path(image_path, Net):
     img = cv.imread(image_path)
     blob = cv.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
@@ -7,7 +8,7 @@ def classify_from_path(image_path, Net):
     return Net.forward()
 
 
-class Net_Yolo():
+class Net_Yolo:
     def __init__(self):
         self.net = cv.dnn.readNet("/workdir/yolov3.weights", "/workdir/darknet/cfg/yolov3.cfg")
         layer_name = self.net.getLayerNames()
