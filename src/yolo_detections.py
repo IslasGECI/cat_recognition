0c24e3a76c75901815f06e50cde1282edfc9fa51
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-20 14:09:24 -0800

    🚧 Move detected cats

--- src/yolo_detections.py
+++ src/yolo_detections.py
@@ -1,10 +1,15 @@
-from os import listdir
+from os import listdir, path, mkdir, replace
 from cat_recognition.yolo_detections import classify_from_path, Net_Yolo, is_there_a_cat
 
 net_yolo = Net_Yolo()
 
 
 def move_photo_with_detections(files):
+    if not path.exists("/workdir/data/cat_detected/"):
+        mkdir("/workdir/data/cat_detected/")
+    old_path = f"/workdir/data/resized/{files}"
+    new_path = f"/workdir/data/cat_detected/{files}"
+    replace(old_path, new_path)
     print(files)
 
 
@@ -12,6 +17,6 @@ def move_photo_with_detections(files):
 images = listdir("data/resized/")
 all_paths = [f"/workdir/data/resized/{image}" for image in images]
 all_outs = [classify_from_path(image_path, net_yolo) for image_path in all_paths]
-for files, outs in zip(all_paths, all_outs):
+for image, outs in zip(images, all_outs):
     if is_there_a_cat(outs):
-        move_photo_with_detections(files)
+        move_photo_with_detections(image)
