669d6a22805ab5da246a9932a116157228f60fcd
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-20 08:32:47 -0800

    🔥Delete object detection box

--- src/yolo_detections.py
+++ src/yolo_detections.py
@@ -39,15 +39,6 @@ for out in outs:
         confidence = scores[class_id]
         if confidence > 0.01:
             # Object detection
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
-            # cv.circle(img, (center_x, center_y), 10, (0, 255, 0), 2 )
-            # Reactangle Cordinate
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
             confidences.append(float(confidence))
             class_ids.append(class_id)
 
