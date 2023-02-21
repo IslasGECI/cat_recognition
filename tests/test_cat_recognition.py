1033342e70b90382ce3f82692e6294a83c8c7050
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-21 09:21:27 -0800

    🛑🧪 Tests `try_resize_image()`

--- tests/test_cat_recognition.py
+++ tests/test_cat_recognition.py
@@ -14,3 +14,11 @@ def test_resize_image():
     obtained_hash = hashlib.md5(image).hexdigest()
     expected_hash = "12f1ac191833ce9a8ad3e9482545176d"
     assert obtained_hash == expected_hash
+
+
+def test_try_resize_image():
+    photos_with_error = []
+    original_image_path = "/workdir/tests/data/photo_with_error.JPG"
+    resized_image_path = "tests/data/image3_resized.jpg"
+    try_resize_image(original_image_path, resized_image_path, photos_with_error)
+    assert len(photos_with_error) == 1
