679c457cacbf4a08ce720e2b8074e45cfecde5f0
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-21 09:36:01 -0800

    ♻️ Use `cat_recognition` module

--- src/resize_images.py
+++ src/resize_images.py
@@ -1,20 +1,8 @@
 import os
 from tqdm import tqdm
 
-from PIL import Image
 
-
-def resize_image(
-    original_path: str,
-    destionation_path: str,
-    width: int = 512,
-    height: int = 512,
-    scale_filter=Image.ANTIALIAS,
-):
-    original_image = Image.open(original_path)
-    resized_image = original_image.resize((width, height), scale_filter)
-    resized_image.save(destionation_path)
-
+from cat_recognition import try_resize_image
 
 original_images_path = "/workdir/data/raw/photos/"
 resized_image_path = "/workdir/data/resized/"
@@ -25,10 +13,7 @@ if not os.path.exists(resized_image_path):
     os.mkdir(resized_image_path)
 images_list_bar = tqdm(images_list)
 for image in images_list_bar:
-    try:
-        resize_image(image, os.path.join(resized_image_path, os.path.split(image)[1]))
-    except:
-        error_photos.append(image)
+    try_resize_image(image, resized_image_path, error_photos)
 
 print("fotos con errores:")
 [print(foto) for foto in error_photos]
