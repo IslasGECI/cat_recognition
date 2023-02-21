44a445533faebfac9495b943d911689fcd60cf35
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-21 11:21:33 -0800

    ♻️ Use `mkdir_resized_image_path()`

--- src/resize_images.py
+++ src/resize_images.py
@@ -2,15 +2,14 @@ import os
 from tqdm import tqdm
 
 
-from cat_recognition import try_resize_image
+from cat_recognition import try_resize_image, mkdir_resized_image_path
 
 original_images_path = "/workdir/data/raw/photos/"
 resized_image_path = "/workdir/data/resized/"
 images_list = [original_images_path + file_name for file_name in os.listdir(original_images_path)]
 
 error_photos = []
-if not os.path.exists(resized_image_path):
-    os.mkdir(resized_image_path)
+mkdir_resized_image_path()
 images_list_bar = tqdm(images_list)
 for image in images_list_bar:
     try_resize_image(image, resized_image_path, error_photos)
