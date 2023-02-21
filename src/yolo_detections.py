9673e2ba06ae7557255fff03288a55deed50c53e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-20 16:24:12 -0800

    🚧 Add waiting bar

--- src/yolo_detections.py
+++ src/yolo_detections.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from os import listdir, path, mkdir, replace
 from cat_recognition.yolo_detections import classify_from_path, Net_Yolo, is_there_a_cat
 
@@ -16,7 +17,7 @@ def move_photo_with_detections(files):
 # Load Image
 images = listdir("data/resized/")
 all_paths = [f"/workdir/data/resized/{image}" for image in images]
-all_outs = [classify_from_path(image_path, net_yolo) for image_path in all_paths]
+all_outs = [classify_from_path(image_path, net_yolo) for image_path in tqdm(all_paths)]
 for image, outs in zip(images, all_outs):
     if is_there_a_cat(outs):
         move_photo_with_detections(image)
