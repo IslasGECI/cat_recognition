c00e04e90ba955e2c880ccecacb0e529d2d42ac9
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-21 10:48:10 -0800

    🛑🧪 Test `obtain_recursive_paths()`

--- tests/test_cat_recognition.py
+++ tests/test_cat_recognition.py
@@ -22,3 +22,16 @@ def test_try_resize_image():
     resized_image_path = "tests/data/image3_resized.jpg"
     cr.try_resize_image(original_image_path, resized_image_path, photos_with_error)
     assert len(photos_with_error) == 1
+
+
+expected_paths = ["/workdir/tests/data/photo_with_error.JPG",
+"/workdir/tests/data/image3_resized.jpg",
+"/workdir/tests/data/image3.jpg",
+"/workdir/tests/data/processed/cat_detected/image3_predicted.jpg",
+"/workdir/tests/data/processed/no_cat_detected/IMG_0178_predicted.jpg",
+"/workdir/tests/data/resized/image3.jpg",]
+
+def tests_obtain_recursive_paths():
+    root_path = "/workdir/tests/data/"
+    obtained_paths = cr.obtain_recursive_paths(root_path)
+    assert obtained_paths == expected_paths
