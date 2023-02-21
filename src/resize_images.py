54ad9c10c061cd99d495c4e01e63e900c9337920
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-21 15:07:26 -0800

    ♻️💬 Use the new interface

--- src/resize_images.py
+++ src/resize_images.py
@@ -2,17 +2,18 @@ import os
 from tqdm import tqdm
 
 
-from cat_recognition import try_resize_image, mkdir_resized_image_path
+from cat_recognition import try_resize_image, mkdir_resized_image_path, obtain_recursive_paths, obtain_output_names
 
 original_images_path = "/workdir/data/raw/photos/"
 resized_image_path = "/workdir/data/resized/"
-images_list = [original_images_path + file_name for file_name in os.listdir(original_images_path)]
+images_list = obtain_recursive_paths(original_images_path)
+new_names = obtain_output_names(original_images_path)
 
 error_photos = []
 mkdir_resized_image_path()
 images_list_bar = tqdm(images_list)
-for image in images_list_bar:
-    try_resize_image(image, resized_image_path, error_photos)
+for image, new_name in zip(images_list_bar, new_names):
+    try_resize_image(image, resized_image_path, new_name, error_photos)
 
 print("fotos con errores:")
 [print(foto) for foto in error_photos]
