420e901e8df087d3c8ebbe9ef715464d182eced3
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-23 10:23:17 -0800

    🧹 Clean auxiliary folders

--- src/make_detections.sh
+++ src/make_detections.sh
@@ -1,1 +1,4 @@
 docker run -itv $PWD:/workdir/data/raw/photos -v $PWD:/workdir/data islasgeci/cat_recognition:latest make detection_with_yolo
+sudo chown --recursive $USER:$USER .
+rm --force --recursive raw
+rm --force --recursive resized
