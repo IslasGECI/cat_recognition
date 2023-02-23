4e57de53d3edc86b3227dfe44623a01bf20d04b4
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-23 12:00:51 -0800

    ✅ Use `move_photo_with_detections()`

--- src/yolo_detections.py
+++ src/yolo_detections.py
@@ -1,17 +1,13 @@
 from tqdm import tqdm
 from os import listdir, path, mkdir, replace
-from cat_recognition.yolo_detections import classify_from_path, Net_Yolo, is_there_a_cat
+from cat_recognition.yolo_detections import (
+    classify_from_path,
+    Net_Yolo,
+    is_there_a_cat,
+    move_photo_with_detections,
+)
 
 net_yolo = Net_Yolo()
-
-
-def move_photo_with_detections(files):
-    if not path.exists("/workdir/data/cat_detected/"):
-        mkdir("/workdir/data/cat_detected/")
-    old_path = f"/workdir/data/resized/{files}"
-    new_path = f"/workdir/data/cat_detected/{files}"
-    replace(old_path, new_path)
-    print(files)
 
 
 # Load Image
@@ -20,4 +16,4 @@ all_paths = [f"/workdir/data/resized/{image}" for image in images]
 all_outs = [classify_from_path(image_path, net_yolo) for image_path in tqdm(all_paths)]
 for image, outs in zip(images, all_outs):
     if is_there_a_cat(outs):
-        move_photo_with_detections(image)
+        move_photo_with_detections(image, "/workdir/data")
