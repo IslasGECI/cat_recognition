e8319f86f296bef93ee17d1ab9fb5aca3767ca6b
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-03 07:59:08 -0800

    😇 Pass test

--- cat_recognition/cat_detector.py
+++ cat_recognition/cat_detector.py
@@ -1,3 +1,13 @@
+from cat_recognition.yolo_detections import (
+    classify_from_path,
+    Net_Yolo,
+    is_there_a_cat,
+    move_photo_with_detections,
+)
+
 class Cat_Detector:
     def __init__(self):
+        self.net_yolo = Net_Yolo()
+
+    def move_if_detection_is_positive(self, image_path, root_path):
         pass
