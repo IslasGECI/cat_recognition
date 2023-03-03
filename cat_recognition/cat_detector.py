68b1f3d96973524ab5c394f8a10e79b6fe2a4709
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-03 08:48:24 -0800

    ✅ Pass test

--- cat_recognition/cat_detector.py
+++ cat_recognition/cat_detector.py
@@ -5,9 +5,14 @@ from cat_recognition.yolo_detections import (
     move_photo_with_detections,
 )
 
+
 class Cat_Detector:
     def __init__(self):
         self.net_yolo = Net_Yolo()
+        self.cut_prob: float = 0.01
 
     def move_if_detection_is_positive(self, image_path, root_path):
-        pass
+        outs = classify_from_path(image_path, self.net_yolo)
+        if is_there_a_cat(outs, self.cut_prob):
+            name_image = image_path.split("/")[-1]
+            move_photo_with_detections(name_image, root_path)
--- tests/test_Cat_Detector.py
+++ tests/test_Cat_Detector.py
@@ -1,20 +1,23 @@
-import pytest
 import os
-import cv2 as cv
-from cat_recognition import (
-    classify_from_path,
-    Net_Yolo,
-    is_there_a_cat,
-    move_photo_with_detections,
-    transform_image_to_blob,
-    Cat_Detector,
-)
+import shutil
+from cat_recognition import Cat_Detector
 
 
 class Test_Cat_Detector:
     def test_cat_detector_init(self):
+        shutil.copyfile(
+            "/workdir/tests/data/resized/IMG_0586.jpg", "/workdir/tests/data/resized/IMG_0586-2.jpg"
+        )
+        output_path = "/workdir/tests/data/cat_detected/IMG_0586-2.jpg"
+        _remove_if_exist(output_path)
         cat_detector = Cat_Detector()
-        image_path = "/workdir/tests/data/resized/IMG_0586.jpg"
-        assert not os.path.exists("/workdir/tests/data/cat_detected/IMG_0586.jpg")
+        image_path = "/workdir/tests/data/resized/IMG_0586-2.jpg"
+        assert not os.path.exists(output_path)
         cat_detector.move_if_detection_is_positive(image_path, "/workdir/tests/data")
-        assert os.path.exists("/workdir/tests/data/cat_detected/IMG_0586.jpg")
+        assert os.path.exists(output_path)
+        _remove_if_exist(output_path)
+
+
+def _remove_if_exist(path):
+    if os.path.exists(path):
+        os.remove(path)
